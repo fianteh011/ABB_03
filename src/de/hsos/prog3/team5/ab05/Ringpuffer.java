ba882838627f6e352ae280f8481a69b38b3aee43
fianteh011 <hafiyyan98@gmail.com>
2021-05-05 20:27:49 +0200

    Initial commit

---
+++ src/de/hsos/prog3/team5/ab05/Ringpuffer.java
@@ -0,0 +1,9 @@
+package de.hsos.prog3.team5.ab05;
+
+import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+public class Ringpuffer<T>{
+}
